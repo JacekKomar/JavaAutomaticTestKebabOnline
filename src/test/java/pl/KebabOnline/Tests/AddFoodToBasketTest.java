5e2b3bc343218ec3f341e484ade744bf88e2d724
JacekKomar <jacek.komar2000@gmail.com>
2023-02-06 15:16:46 +0100

    Add test of plus and dash buttons in FoodsPage.

--- src/test/java/pl/KebabOnline/Tests/AddFoodToBasketTest.java
+++ src/test/java/pl/KebabOnline/Tests/AddFoodToBasketTest.java
@@ -7,34 +7,52 @@ import pl.KebabOnline.Utils.Main;
 
 public class AddFoodToBasketTest extends Main {
 
-
-
-
-    public void foodButton(String label){
+    public void foodAddButton(String label){
         driver.findElement(By.xpath("(//i[@class=\"bi bi-plus-square\"])" + label +"")).click();
 
     }
+    public void foodDeleteButton(String label){
+        driver.findElement(By.xpath("(//i[@class=\"bi bi-dash-square\"])" + label +"")).click();
 
-
+    }
 
     @Test(priority = 1)
     public void addFoodToBasket() {
         FoodPage FoodPage = new FoodPage(driver);
         FoodPage.openKebabFoodPageClick();
-        foodButton("[2]");
-
+        foodAddButton("[2]");
+        foodAddButton("[2]");
+        foodAddButton("[1]");
+        foodAddButton("[3]");
+        foodAddButton("[9]");
+        foodAddButton("[5]");
+        foodAddButton("[6]");
+        foodAddButton("[8]");
+        foodAddButton("[7]");
+        foodAddButton("[4]");
+        foodAddButton("[9]");
+        foodAddButton("[1]");
+        foodAddButton("[2]");
     }
 
     @Test(priority = 2)
+    public void deleteFood(){
+        foodDeleteButton("[1]");
+        foodDeleteButton("[2]");
+        foodDeleteButton("[3]");
+        foodDeleteButton("[4]");
+        foodDeleteButton("[5]");
+        foodDeleteButton("[6]");
+        foodDeleteButton("[7]");
+        foodDeleteButton("[8]");
+        foodDeleteButton("[9]");
+
+    }
+
+    @Test(priority = 3)
     public void exitFoodPage(){
         FoodPage FoodPage = new FoodPage(driver);
         FoodPage.orderFoodButtonClick();
     }
 
-
-
-
-
-
 }
-
