fe759d78dd8b9ab5a926a81d479c461bc543dfb8
JacekKomar <jacek.komar2000@gmail.com>
2023-02-08 15:03:48 +0100

    Add order Test.

--- src/test/java/pl/KebabOnline/Pages/OrderPage.java
+++ src/test/java/pl/KebabOnline/Pages/OrderPage.java
@@ -1,6 +1,42 @@
 package pl.KebabOnline.Pages;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class OrderPage {
 
-//    (//i[@class="bi bi-plus-square"])[15]
+    @FindBy(xpath = "(//input[@id='exampleInputEmail1'])")
+    WebElement name;
+
+    @FindBy(xpath = "(//input[@id='Username'])")
+    WebElement username;
+
+    @FindBy(xpath = "(//input[@id='Adress'])")
+    WebElement adress;
+
+    @FindBy(xpath = "(//input[@id='Phone'])")
+    WebElement phone;
+
+    @FindBy(xpath = "(//input[@id='info'])")
+    WebElement info;
+
+    @FindBy(xpath = "(//button[contains(text(),'Zamów')])")
+    WebElement orderButton;
+
+    public OrderPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void addTextToInput() {
+    name.sendKeys("Jacek");
+    username.sendKeys("Komar");
+    adress.sendKeys("Jasionka 31");
+    phone.sendKeys("123321149");
+    info.sendKeys("Sos: mieszany, mięso: baranina");
+    orderButton.click();
+    }
+
+
+
 }
--- src/test/java/pl/KebabOnline/Tests/OrderTest.java
+++ src/test/java/pl/KebabOnline/Tests/OrderTest.java
@@ -1,9 +1,17 @@
 package pl.KebabOnline.Tests;
 
 import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import pl.KebabOnline.Pages.FoodPage;
+import pl.KebabOnline.Pages.OrderPage;
 
 public class OrderTest extends Main {
 
+    @Test(priority = 1)
+    public void basket(){
+        OrderPage orderPage = new OrderPage(driver);
+        orderPage.addTextToInput();
+    }
 
 
 }
